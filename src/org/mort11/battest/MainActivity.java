2b583b51ca6d9ed0c35bbd3c945a8d5bb94cd361
Will Marshall <wmarshall@mort11.org>
2013-12-26 19:32:46 -0500

    working sync

--- src/org/mort11/battest/MainActivity.java
+++ src/org/mort11/battest/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.LocalBroadcastManager;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -16,53 +19,81 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    
+
 	private boolean running = false;
 	private int syncs = 0;
+
+	private final Handler timerHandler = new Handler();
+	private Time startTime = new Time("UTC");
+	private Time lastRunTime = new Time("UTC"); 
+	private final Runnable timer = new Runnable() {
+
+		@Override
+		public void run() {
+			Time now = new Time("UTC");
+			now.setToNow();
+			Time delta = new Time("UTC");
+			delta.set(now.toMillis(true) - startTime.toMillis(true));
+			((TextView) findViewById(R.id.timeElapsed)).setText(delta
+					.format("%H:%M:%S"));
+			timerHandler.postDelayed(this, 500);
+		}
+
+	};
 
 	private final Intent enableBTIntent = new Intent(
 			BluetoothAdapter.ACTION_REQUEST_ENABLE);
 	private final int SEL_PARTNER = 1;
 	private final int START_TEST = 2;
-	private final int STOP_TEST = 3;
+	private final int START_TEST_SERVER = 3;
 
-    private final IntentFilter BTfilter = new IntentFilter(BluetoothDevicePicker.ACTION_DEVICE_SELECTED);
+	private final IntentFilter BTfilter = new IntentFilter(
+			BluetoothDevicePicker.ACTION_DEVICE_SELECTED);
 	private final BluetoothDeviceSelectedReceiver deviceSelectedRX = new BluetoothDeviceSelectedReceiver();
-    
-	private final Intent launchDevicePicker = new Intent(BluetoothDevicePicker.ACTION_LAUNCH)
-        .putExtra(BluetoothDevicePicker.EXTRA_NEED_AUTH, false)
-        .putExtra(BluetoothDevicePicker.EXTRA_FILTER_TYPE, BluetoothDevicePicker.FILTER_ALL);
-	
 
+	private final Intent launchDevicePicker = new Intent(
+			BluetoothDevicePicker.ACTION_LAUNCH).putExtra(
+			BluetoothDevicePicker.EXTRA_NEED_AUTH, false).putExtra(
+			BluetoothDevicePicker.EXTRA_FILTER_TYPE,
+			BluetoothDevicePicker.FILTER_ALL);
+
+	private final IntentFilter statusUpdates = new IntentFilter(
+			SyncClientService.statusUpdate.getAction());
 	private final BroadcastReceiver syncUpdate = new BroadcastReceiver() {
-		public void onReceive(Context c, Intent i ){
-			if(i == SyncClientService.statusUpdate){
+		public void onReceive(Context c, Intent i) {
+			Log.i(getString(R.string.LogcatTag), "Success = " + i.getBooleanExtra("success", true));
+			if (i.getBooleanExtra("success", true)) {
 				syncs++;
-			}else{
-				if(running){
+				((TextView) findViewById(R.id.numSyncs)).setText(syncs
+						+ " syncs");
+				((TextView) findViewById(R.id.syncStatus))
+						.setText(R.string.success);
+			} else {
+				if (running) {
 					toggleTesting(findViewById(R.id.startTest));
+					((TextView) findViewById(R.id.syncStatus))
+							.setText(R.string.failed);
 				}
 			}
-		} 
+		}
 	};
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-//		registerReceiver(deviceSelectedRX, BTfilter);
+		LocalBroadcastManager.getInstance(this).registerReceiver(syncUpdate, statusUpdates);
+		Time last = new Time("UTC");
+		last.set(getPreferences(MODE_PRIVATE).getLong("Last Run", 0));
+		((TextView) findViewById(R.id.timeElapsed)).setText(last
+				.format("%H:%M:%S"));
 	}
 
 	@Override
-	protected void onResume() {
-		
-		super.onResume();
-	}
-
-	@Override
-	protected void onPause() {
-//		unregisterReceiver(deviceSelectedRX);
-		super.onPause();
+	protected void onStop() {
+		getPreferences(MODE_PRIVATE).edit().putLong("Last Run",
+				lastRunTime.toMillis(true)).apply();
+		super.onStop();
 	}
 
 	@Override
@@ -79,7 +110,11 @@ public class MainActivity extends Activity {
 
 	public void toggleTesting(View v) {
 		if (((Switch) findViewById(R.id.server)).isChecked()) {
-			// pass
+			if (!running) {
+				initBluetooth(START_TEST_SERVER);
+			} else {
+				stopTest(true);
+			}
 		} else {
 			if (deviceSelectedRX.getPartner() == null) {
 				Toast.makeText(this, "need partner", Toast.LENGTH_SHORT).show();
@@ -88,42 +123,27 @@ public class MainActivity extends Activity {
 			if (!running) {
 				initBluetooth(START_TEST);
 			} else {
-				getWindow().clearFlags(
-						WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-				stopService(new Intent(this, SyncClientService.class));
-				running = false;
-				((TextView) v).setText(R.string.stopTest);
+				stopTest(false);
 			}
 		}
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		switch (requestCode) {
-		case SEL_PARTNER:
-			if (resultCode == RESULT_CANCELED) {
-				Toast.makeText(this, R.string.turnOnBT, Toast.LENGTH_SHORT)
-						.show();
-			} else {
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			case SEL_PARTNER:
 				scanForPartners();
+				break;
+			case START_TEST:
+				startTest(false);
+				break;
+			case START_TEST_SERVER:
+				startTest(true);
+				break;
 			}
-			break;
-		case START_TEST:
-			if (resultCode == RESULT_CANCELED) {
-				Toast.makeText(this, R.string.turnOnBT, Toast.LENGTH_SHORT)
-						.show();
-			} else {
-				getWindow().addFlags(
-						WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-				Intent syncClientIntent = new Intent(this,
-						SyncClientService.class).putExtra("MAC",
-						deviceSelectedRX.getPartner().getAddress());
-				startService(syncClientIntent);
-				syncs = 0;
-				running = true;
-				
-			}
-			break;
+		} else {
+			Toast.makeText(this, R.string.turnOnBT, Toast.LENGTH_SHORT).show();
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
@@ -132,15 +152,47 @@ public class MainActivity extends Activity {
 		BluetoothAdapter mbtAdapter = BluetoothAdapter.getDefaultAdapter();
 		if (!mbtAdapter.isEnabled()) {
 			startActivityForResult(enableBTIntent, reqCode);
-		}else{
-            scanForPartners();
-        }
+		} else {
+			onActivityResult(reqCode, RESULT_OK, null);
+		}
 	}
 
 	private void scanForPartners() {
 		registerReceiver(deviceSelectedRX, BTfilter);
 		startActivity(launchDevicePicker);
-        Log.d(getString(R.string.LogcatTag), "asked for partner");
+		Log.i(getString(R.string.LogcatTag), "asked for partner");
+	}
+
+	private void startTest(boolean server) {
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		if (server) {
+			Intent syncServerIntent = new Intent(this, SyncServerService.class);
+			startService(syncServerIntent);
+		} else {
+			Intent syncClientIntent = new Intent(this, SyncClientService.class)
+					.putExtra("MAC", deviceSelectedRX.getPartner().getAddress());
+			startService(syncClientIntent);
+		}
+		syncs = 0;
+		running = true;
+		startTime.setToNow();
+		timerHandler.post(timer);
+		((TextView) findViewById(R.id.startTest)).setText(R.string.stopTest);
+	}
+
+	private void stopTest(boolean server) {
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		if (server) {
+			stopService(new Intent(this, SyncServerService.class));
+		} else {
+			stopService(new Intent(this, SyncClientService.class));
+		}
+		running = false;
+		Time now = new Time("UTC");
+		now.setToNow();
+		lastRunTime.set(now.toMillis(true) - startTime.toMillis(true));
+		timerHandler.removeCallbacks(timer);
+		((TextView) findViewById(R.id.startTest)).setText(R.string.startTest);
 	}
 
 }
--- src/org/mort11/battest/SyncServerService.java
+++ src/org/mort11/battest/SyncServerService.java
@@ -6,61 +6,58 @@ import java.util.UUID;
 
 import android.app.IntentService;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
-public class SyncClientService extends IntentService {
-	protected final UUID syncUUID = UUID.fromString("439a369c-5b92-48c2-90e7-a4a94dd4cea2"); 
+public class SyncServerService extends IntentService {
+
+	private BluetoothSocket syncSock = null;
+	private BluetoothServerSocket servSock = null;
+	
+	protected final UUID syncUUID = UUID
+			.fromString("439a369c-5b92-48c2-90e7-a4a94dd4cea2");
+
 	protected Random rand;
 
 	public static final Intent statusUpdate = new Intent(
 			"org.mort11.battest.SYNCED");
-	public static final Intent failed = new Intent("org.mort11.battest.FAIL");
 
-	private BluetoothSocket sock = null;
-	
-	public SyncClientService() {
-		super(SyncClientService.class.getName());
+	public SyncServerService() {
+		super(SyncServerService.class.getName());
 		rand = new Random();
 	}
 
 	@Override
-	protected void onHandleIntent(Intent intent) {
-		BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
-		BluetoothDevice server = BluetoothAdapter.getDefaultAdapter()
-				.getRemoteDevice(intent.getStringExtra("MAC"));
-//		server.fetchUuidsWithSdp();
-		
+	protected void onHandleIntent(Intent arg0) {
+		BluetoothAdapter mAdapter = BluetoothAdapter.getDefaultAdapter();
+		mAdapter.cancelDiscovery();
 		try {
+			servSock = mAdapter.listenUsingRfcommWithServiceRecord(
+					"StupidSync", syncUUID);
 			while (true) {
-				sock = server
-						.createRfcommSocketToServiceRecord(syncUUID);
-				Log.i(getString(R.string.LogcatTag), "Connecting");
-				sock.connect();
+				syncSock = servSock.accept();
 				Log.i(getString(R.string.LogcatTag), "Connected");
 				byte[] buffer = new byte[1024];
 				for (int i = 0; i < 50; i++) {
 					rand.nextBytes(buffer);
-					sock.getOutputStream().write(buffer);
+					syncSock.getOutputStream().write(buffer);
 				}
-				Log.i(getString(R.string.LogcatTag), "Sent Data");
 				int bytesRX = 0;
 				while (bytesRX < 50 * 1024) {
-					int thisread = sock.getInputStream().read(buffer);
+					int thisread = syncSock.getInputStream().read(buffer);
 					if(thisread <= 0){
 						throw new IOException("HUP");
 					}
 					bytesRX += thisread;
 				}
-				Log.i(getString(R.string.LogcatTag), "Read Data");
-				sock.getOutputStream().write("DONE".getBytes());
+				syncSock.getOutputStream().write("DONE".getBytes());
 				bytesRX = 0;
 				while (bytesRX < 4) {
 					try {
-						if(sock.getInputStream().read() == -1){
+						if(syncSock.getInputStream().read() == -1){
 							throw new IOException("HUP");
 						}
 						bytesRX++;
@@ -68,36 +65,41 @@ public class SyncClientService extends IntentService {
 						break;
 					}
 				}
-				Log.i(getString(R.string.LogcatTag), "DONE");
 				try {
-					sock.close();
+					syncSock.close();
 				} catch (IOException e) {
 
 				}
 				LocalBroadcastManager.getInstance(this).sendBroadcast(
 						statusUpdate.putExtra("success", true));
-				Thread.sleep(1000 * 60 * 3);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
 		}
+
 	}
 
 	@Override
 	public void onDestroy() {
 		LocalBroadcastManager.getInstance(this).sendBroadcast(
 				statusUpdate.putExtra("success", false));
-		if(sock != null){
+		if(syncSock != null ){
 			try {
-				sock.close();
+				syncSock.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if(servSock != null){
+			try {
+				servSock.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		super.onDestroy();
-		Log.i(getString(R.string.LogcatTag), "EXITING CLIENT");
+		Log.i(getString(R.string.LogcatTag), "EXITING SERVER");
+		
 	}
 
 }
